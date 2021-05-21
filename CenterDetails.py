264b68966d19693a4e3549216bea353b1518c9b9
ashit kumar <ashit.lnct@gmail.com>
2021-05-21 13:38:04 +0530

    fix to sent  telegram msg only once per updated data in center and connection error logging

--- CenterDetails.py
+++ CenterDetails.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 class CenterInfo():
     def __init__(self, name = None, blockname = None, pincode = None,  feeType = None,
-     capacity = None, dose1 = None, dose2 = None, vaccine = None, ageLimit = None, date = None):
+     capacity = None, dose1 = None, dose2 = None, sessionId = None, vaccine = None, ageLimit = None, date = None):
        self.name = name
        self.blockName = blockname
        self.pincode = pincode
@@ -9,6 +9,7 @@ class CenterInfo():
        self.capacity = capacity
        self.dose1 = dose1
        self.dose2 = dose2
+       self.sessionId = sessionId
        self.vaccine = vaccine
        self.ageLimit = ageLimit
        self.date = date
@@ -22,6 +23,7 @@ class CenterInfo():
         print("capacity: " , self.capacity)
         print("dose 1: " , self.dose1)
         print("dose 2: ",  self.dose2)
+        print("sessionId", self.sessionId)
         print("vaccine: " , self.vaccine)
         print("age limit: " , self.ageLimit)
         print("date:" , self.date)
--- vaccine-availability-finder-dev.py
+++ vaccine-availability-finder-dev.py
@@ -10,82 +10,113 @@ import time
 import logging
 logging.basicConfig(filename='myapp.log', format='%(asctime)s : %(levelname)s - %(message)s', level=logging.INFO, datefmt='%m/%d/%Y %I:%M:%S %p' )
 
+session_id = "none"
+centerList_Global =[]
+
 def cowinApiCall():
-    logging.info('-----Started -------')
+    logging.info('-------------Started ---------------')
     temp_user_agent = UserAgent()
     browser_header = {'User-Agent': temp_user_agent.random}
     systemDate = datetime.today().strftime('%d-%m-%Y')
     district_id = 294  # 294- BBMP
     age = 32
     centerList = []
+    global session_id
+    global centerList_Global
 
     getStatesListUrl = "https://cdn-api.co-vin.in/api/v2/admin/location/states"
     calendarByDistrictUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict"
 
     queryparam = {'district_id': district_id, 'date': systemDate}
 
-    response = requests.get(calendarByDistrictUrl,
-                            headers=browser_header, params=queryparam)
-    logging.info('response: '+ str(response))
-    if response.ok:
-        resp_json = response.json()
-        if resp_json["centers"]:
-            logging.info('Available on: '+ str(systemDate) +
-                ' for 18-45 age group ,user age: ' + str(age))
-            for center in resp_json["centers"]:
-                for session in center["sessions"]:
-                    if session["min_age_limit"] <= age:
-                        name = center["name"]
-                        block = center["block_name"]
-                        pincode = center["pincode"]
-                        feeType = center["fee_type"]
-                        capacity = session["available_capacity"]
-                        dose1 = session["available_capacity_dose1"]
-                        dose2 = session["available_capacity_dose2"]
-                        logging.info('\t ' + center["name"])
-                        logging.info('\t ' + center["block_name"])
-                        logging.info('\t Price: ' +  center["fee_type"])
-                        logging.info('\t Available Capacity: ' +
-                            str(session["available_capacity"]))
-                        logging.info('\t Available Dose 1: ' +
-                            str(session["available_capacity_dose1"]))
-                        logging.info('\t Available Dose 2: ' +
-                            str(session["available_capacity_dose2"]))
-                        if(session["vaccine"] != ''):
-                            logging.info('\t Vaccine: ' + session["vaccine"])
-                            vaccine = session["vaccine"]
-                        logging.info('\t min age limit : ' +
-                              str(session["min_age_limit"]))
-                        logging.info('\t date: ' +
-                              str(session["date"]))
-                        ageLimit = session["min_age_limit"]
-                        date = session["date"]
-                        logging.info('----------------------------------- \n\n ')
-                        centerList.append(CenterInfo(name, block, pincode, feeType, capacity, dose1, dose2, vaccine,ageLimit, date))
-        else:
-            logging.info("No available centers on ", systemDate)
+    try:
+        response = requests.get(calendarByDistrictUrl,
+                                headers=browser_header, params=queryparam)
+        logging.info('response: '+ str(response))
+        if response.ok:
+            resp_json = response.json()
+            if resp_json["centers"]:
+                logging.info('Available on: '+ str(systemDate) +
+                    ' for 18-45 age group ,user age: ' + str(age))
+                for center in resp_json["centers"]:
+                    for session in center["sessions"]:
+                        if session["min_age_limit"] <= age:
+                            name = center["name"]
+                            block = center["block_name"]
+                            pincode = center["pincode"]
+                            feeType = center["fee_type"]
+                            capacity = session["available_capacity"]
+                            dose1 = session["available_capacity_dose1"]
+                            dose2 = session["available_capacity_dose2"]
+                            logging.info('\t ' + center["name"])
+                            logging.info('\t ' + center["block_name"])
+                            logging.info('\t Price: ' +  center["fee_type"])
+                            logging.info('\t Available Capacity: ' +
+                                str(session["available_capacity"]))
+                            logging.info('\t Available Dose 1: ' +
+                                str(session["available_capacity_dose1"]))
+                            logging.info('\t Available Dose 2: ' +
+                                str(session["available_capacity_dose2"]))
+                            sessionId = session["session_id"]
+                            logging.info('\t Available Dose 2: ' +
+                                        str(session["session_id"]))
+                            if(session["vaccine"] != ''):
+                                logging.info('\t Vaccine: ' + session["vaccine"])
+                                vaccine = session["vaccine"]
+                            logging.info('\t min age limit : ' +
+                                str(session["min_age_limit"]))
+                            logging.info('\t date: ' +
+                                str(session["date"]))
+                            ageLimit = session["min_age_limit"]
+                            date = session["date"]
+                            logging.info('----------------------------------- \n\n ')
+                            centerList.append(CenterInfo(name, block, pincode, feeType, capacity, dose1, dose2, sessionId, vaccine, ageLimit, date))
+            else:
+                logging.info("No available centers on ", systemDate)
 
-    for center in centerList:
-        if center.capacity > 0:
-            logging.info(' slots available - sending telegram notification:  center name : ' + str(center.name) +
-                         ' capacity: ' + str(center.capacity) + ' pincode: ' + str(center.pincode))
-            telegram_bot_sendtext("Center : " + center.name + "\n"
-                              + "Block : " + center.blockName + "\n"
-                              + "pincode : " + str(center.pincode) + "\n"
-                              + "fee type : " + str(center.feeType) + "\n"
-                              + "available capacity : " + str(center.capacity) + "\n"
-                              + "available Dose1 : " + str(center.dose1) + "\n"
-                              + "available Dose2 : " + str(center.dose2) + "\n"
-                              + "vaccine : " + str(center.vaccine) + "\n"
-                              + "age limit : " + str(center.ageLimit) + "\n"
-                              + "Date : " + str(center.date) + "\n")
-        else:
-            telegram_bot_sendtext("No vaccine available at center " + center.name)
+        for center in centerList:
+            if center.capacity == 0 and isCenterDetailsUpdated(center):
+                logging.info(' slots available - sending telegram notification:  center name : ' + str(center.name) +
+                            ' Global session id : ' + str(session_id) + ' sessionId: ' + str(center.sessionId))
+                telegram_bot_sendtext("Center : " + center.name + "\n"
+                                + "Block : " + center.blockName + "\n"
+                                + "pincode : " + str(center.pincode) + "\n"
+                                + "fee type : " + str(center.feeType) + "\n"
+                                + "available capacity : " + str(center.capacity) + "\n"
+                                + "available Dose1 : " + str(center.dose1) + "\n"
+                                + "available Dose2 : " + str(center.dose2) + "\n"
+                                + "vaccine : " + str(center.vaccine) + "\n"
+                                + "age limit : " + str(center.ageLimit) + "\n"
+                                + "Date : " + str(center.date) + "\n")
+            else:
+                #telegram_bot_sendtext("No vaccine available at center " + center.name)
+                logging.info("No vaccine available at center " + center.name)
 
-    #for sending telegram notification
-    #test = telegram_bot_sendtext("Hi Ashit ")
-    #print(test)
+            centerList_Global = centerList
 
+    except requests.ConnectionError as  err:
+        logging.error("Connection error : " + str(err))
+
+def isCenterDetailsUpdated(center):
+    global centerList_Global
+    if centerList_Global:
+        for savedCenter in centerList_Global:
+            #unique id for centername (center name + date)
+            savedId = savedCenter.name + savedCenter.date
+            centerId= center.name + center.date
+            logging.info("savedId: " + savedId + " centerId: " + centerId)
+            if savedId == centerId and savedCenter.sessionId != center.sessionId:
+                logging.info("ID Updated - send notification ")
+                return True
+            else:
+                logging.info("ID Not Updated- No notification ")
+                return False
 
 
 cowinApiCall()
+
+schedule.every(30).seconds.do(cowinApiCall)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
