00be890a8832b04a4550c00d6b64df30bc4b20ba
ashit kumar <ashit.lnct@gmail.com>
2021-05-30 15:58:43 +0530

    blore urban 30sec

--- telegram_bot_test_env.py
+++ telegram_bot_test_env.py
@@ -5,7 +5,7 @@ import schedule
 
 
 def telegram_bot_sendtext(bot_message, bot_chatID):
-    bot_token = '1755134661:AAE4ysHFi75lUqF2o2H_Fs3Or3jHDlfKUjM'
+    bot_token = '1755134661:AAHAXJhDZfdinst1gzqp6MCpT9EAnYKDBUY'
     # default chat id , cowin U45 Blore-Dev chat id
     #bot_chatID = '-1001172971393'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
